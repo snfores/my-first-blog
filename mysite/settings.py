d008a52e182880b3fcf966a1cd4c695d7c25a400
Kitamori Shinji <sin1116twin@gmail.com>
2019-12-21 14:19:23 +0900

    add settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# ログインページに直接アクセスして、ログインが成功したときにトップレベルのインデックス（ブログのホームページ）にリダイレクトします。
-LOGIN_REDIRECT_URL = '/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#?
+# ログインページに直接アクセスして、ログインが成功したときにトップレベルのインデックス（ブログのホ>
+LOGIN_REDIRECT_URL = '/'
